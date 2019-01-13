afc1341e173ab0aca5b0f70e593d25bb846a2e30
Suguru ARAKI <sgr.araki@gmail.com>
2019-01-13 23:08:06 +0900

    Solve problems of TypingML4 (Q97, Q98)

--- 08/Q98.ml
+++ 08/Q98.ml
@@ -3,15 +3,49 @@
    let k2 = fun x -> fun y -> x in
    s k1 k2 (fun x -> x + 1) : int -> int
 by T-Let {
-    |- fun f -> fun g -> fun x -> f x (g x) : ((int -> int) -> (int -> int) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) by T-Fun {
-        f : ((int -> int) -> (int -> int) -> (int -> int)) |- fun g -> fun x -> f x (g x) : ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) by T-Fun {
-            f : ((int -> int) -> (int -> int) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)) |- fun x -> f x (g x) : (int -> int) -> (int -> int) by T-Fun {
-                f : ((int -> int) -> (int -> int) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- f x (g x) : (int -> int) by T-App {
-                    f : ((int -> int) -> (int -> int) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- f x : (int -> int) -> (int -> int) by T-App {};
-                    f : ((int -> int) -> (int -> int) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- g x : (int -> int) -> (int -> int) by T-App {};
+    |- fun f -> fun g -> fun x -> f x (g x) : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) by T-Fun {
+        f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) |- fun g -> fun x -> f x (g x) : ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) by T-Fun {
+            f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)) |- fun x -> f x (g x) : (int -> int) -> (int -> int) by T-Fun {
+                f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- f x (g x) : (int -> int) by T-App {
+                    f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- f x : ((int -> int) -> (int -> int)) -> (int -> int) by T-App {
+                        f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- f : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int) by T-Var {};
+                        f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- x : (int -> int) by T-Var {};
+                    };
+                    f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- g x : (int -> int) -> (int -> int) by T-App {
+                        f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- g : (int -> int) -> (int -> int) -> (int -> int) by T-Var {};
+                        f : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)), g : ((int -> int) -> (int -> int) -> (int -> int)), x : int -> int |- x : (int -> int) by T-Var {};
+                    };
                 };
             };
         };
     };
-    s : ((int -> int) -> (int -> int) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) |- let k1 = fun x -> fun y -> x in let k2 = fun x -> fun y -> x in s k1 k2 (fun x -> x + 1) by T-Let {};
+    s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) |- let k1 = fun x -> fun y -> x in let k2 = fun x -> fun y -> x in s k1 k2 (fun x -> x + 1) : int -> int by T-Let {
+        s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int) |- fun x -> fun y -> x : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int) by T-Fun {
+            s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), x : int -> int |- fun y -> x : ((int -> int) -> (int -> int)) -> (int -> int) by T-Fun {
+                s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), x : int -> int, y : (int -> int) -> (int -> int) |- x : (int -> int) by T-Var {};
+            };
+        };
+        s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int) |- let k2 = fun x -> fun y -> x in s k1 k2 (fun x -> x + 1) : int -> int by T-Let {
+            s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int) |- fun x -> fun y -> x : (int -> int) -> (int -> int) -> (int -> int) by T-Fun {
+                s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), x : int -> int |- fun y -> x : (int -> int) -> (int -> int) by T-Fun {
+                    s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), x : int -> int, y : int -> int |- x : (int -> int) by T-Var {};
+                };
+            };
+            s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- s k1 k2 (fun x -> x + 1) : int -> int by T-App {
+                s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- s k1 k2 : (int -> int) -> int -> int by T-App {
+                    s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- s k1 : ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> int -> int by T-App {
+                        s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> int -> int by T-Var {};
+                        s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- k1 : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) by T-Var {};
+                    };
+                    s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- k2 : ((int -> int) -> (int -> int) -> (int -> int)) by T-Var {};
+                };
+                s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int) |- fun x -> x + 1 : int -> int by T-Fun {
+                    s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int), x : int |- x + 1 : int by T-Plus {
+                        s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int), x : int |- x : int by T-Var {};
+                        s : ((int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int)) -> ((int -> int) -> (int -> int) -> (int -> int)) -> (int -> int) -> (int -> int), k1 : (int -> int) -> ((int -> int) -> (int -> int)) -> (int -> int), k2 : (int -> int) -> (int -> int) -> (int -> int), x : int |- 1 : int by T-Int {};
+                    };
+                };
+            };
+        };
+    };
 }   
